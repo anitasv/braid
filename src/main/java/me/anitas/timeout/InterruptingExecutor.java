382d9c4c7d2556f44174ba4c8287337dfbb094b5
anitasv <anitasvasu@gmail.com>
2014-10-06 01:46:26 +0530

    SImplify logic further

--- src/main/java/me/anitas/timeout/InterruptingExecutor.java
+++ src/main/java/me/anitas/timeout/InterruptingExecutor.java
@@ -1,6 +1,7 @@
 package me.anitas.timeout;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class InterruptingExecutor implements Executor {
 
@@ -8,35 +9,24 @@ public class InterruptingExecutor implements Executor {
 
     private final ScheduledExecutorService scheduledExecutorService;
 
-    private class Interrupter implements Callable<Boolean> {
+    private class Interrupter implements Runnable {
 
         private final Thread thread;
+
+        private final AtomicBoolean done = new AtomicBoolean(true);
 
         private Interrupter(Thread thread) {
             this.thread = thread;
         }
 
         @Override
-        public Boolean call() throws Exception {
-            if (Thread.interrupted()) {
-                // We are able to detect a cancellation early enough to prevent an interrupt
-                // the check also clears the interrupt on this thread, so we are okay
-                return false;
-            } else {
-                // We have not yet received cancellation, even though it might be cancelled by
-                // now, there are three cases
-                //    a) either we return true, in which case the original thread will
-                //       clear interrupt status
-                //    b) the get() call pending itself got interrupted, in that case scheduledExecutorService
-                //       will clear the interrupt status when creating the InterruptedException.
-                //    c) The cancel appeared after processing, but interrupt on this thread was raised, this
-                //       will also be cleared by the scheduledExecutorService when it sees callable has
-                //       returned and status has not changed.
+        public void run() {
+            if (done.compareAndSet(false, true)) {
                 thread.interrupt();
-                return true;
             }
         }
     }
+
 
     public InterruptingExecutor(int corePoolSize, long timeout, TimeUnit timeUnit) {
         this.timeoutNanos = timeUnit.toNanos(timeout);
@@ -45,28 +35,23 @@ public class InterruptingExecutor implements Executor {
 
     @Override
     public void execute(Runnable command) {
-        Future<Boolean> interrupter =
-                scheduledExecutorService.schedule(new Interrupter(Thread.currentThread()),
+
+        Interrupter actualInterrupter = new Interrupter(Thread.currentThread());
+
+        Future<?> interrupter =
+                scheduledExecutorService.schedule(actualInterrupter,
                         timeoutNanos, TimeUnit.NANOSECONDS);
         try {
             command.run();
         } finally {
             // If interrupter doesn't clear this status scheduledExecutorService will
             interrupter.cancel(true);
-            try {
-                interrupter.get();
-            } catch (InterruptedException e) {
-                // The interrupt appeared after cancel was issued, but before interrupter could
-                // detect that it was interrupted.
-                // throwing an interrupted exception automatically must clear interruption status
-                // so this is also taken care of.
-            } catch (ExecutionException e) {
-                // Must not happen
-            } catch (CancellationException e) {
-                // Cancelled before scheduling.
-            } finally {
-                Thread.interrupted();
-            }
+
+            // Make sure you eat the capsule so that no additional interrupt is possible
+            actualInterrupter.run();
+
+            // Claer now.
+            Thread.interrupted();
         }
     }
 }
